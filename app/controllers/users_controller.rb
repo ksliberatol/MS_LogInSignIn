97bb3e30f4f04e8cf72c89366670a02adb0b92f0
Kevin <ksliberatol@unal.edu.co>
2020-11-20 01:23:20 -0500

    2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show]
+  #before_action :set_user, only: [:show]
 
   def show 
 	  @user = user = User.find(params[:email])
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
     end
   
     def user_params
-      params.require(:user).permit(:id)
+      params.require(:user).permit(:email)
     end
 end

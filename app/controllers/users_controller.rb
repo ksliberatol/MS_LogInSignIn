83d0c1574bd8b7f2d067e142096f1066f06afeae
Kevin <ksliberatol@unal.edu.co>
2020-11-20 01:26:02 -0500

    3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   #before_action :set_user, only: [:show]
 
   def show 
-	  @user = user = User.find(params[:email])
+	  @user = User.find(params[:email])
     render json: @user 
     #render :json=> @user.as_json(:tokens=>@user.tokens)
     #render :json=> @user.as_json(:token=>@user.tokens)
